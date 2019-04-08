abd000b9f450b7d3542866b12b7ec1e1d06ffaf6
cperry10 <cperry10@unix4.csc.calpoly.edu>
2019-04-08 12:27:55 -0700

    Completed Location andlocation_tests functions, by adding eq and repr method to Location object

--- location.py
+++ location.py
@@ -1,5 +1,5 @@
 # CPE 202 Lab 0
-
+from math import *
 # represents a location using name, latitude and longitude
 class Location:
     def __init__(self, name, lat, lon):
@@ -8,6 +8,13 @@ class Location:
         self.lon = lon      # longitude in degrees (-180 to 180)
 
 # ADD BOILERPLATE HERE (__eq__ and __repr__ functions)
+    def __eq__(self, other):
+        return (type(other) == Location and isclose(self.lat, other.lat) 
+        and isclose(self.lon, other.lon))
+
+    def __repr__(self):
+        return "Location(\'{0}\', {1}, {2})".format(self.name,
+        str(self.lat), str(self.lon))
 
 def main():
     loc1 = Location("SLO", 35.3, -120.7)
